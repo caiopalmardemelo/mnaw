7334e30e62e0298860bfedd479d7b9fb6bb5ee46
caiopalmardemelo <159266607+caiopalmardemelo@users.noreply.github.com>
2025-03-10 08:41:49 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 
 int main(){
@@ -19,3 +11,19 @@ int main(){
 }
 
 }
+*******************************************************************************
+int main(){
+
+    int i;
+    i = -1;
+
+    while (i < 20){
+        i = i + 1;
+        if(i%2==0){
+            printf(" %d",i);
+}
+}
+
+}
+*******************************************************************************
+
