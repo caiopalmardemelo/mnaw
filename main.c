6eba42c429b1324df70b81e901a36500ea962c39
caiopalmardemelo <159266607+caiopalmardemelo@users.noreply.github.com>
2025-03-11 11:15:37 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,15 @@
-#include <stdio.h>
-
+/*escreva um programa que imprima os números de 1 a 10 usando um loop while*/
 int main(){
 
     int i;
     i = 0;
 
-    while(i<10){
-    printf("%d ",i);
+    while( i <= 10 ){
+    printf(" %d ", i );
     i++;
     }
 }
-*******************************************************************************
+/*faça um programa que conte de 0 a 20, mas só imprima os números pares*/
 int main(){
 
     int i;
@@ -18,10 +17,22 @@ int main(){
 
     while (i < 20){
         i = i + 1;
-        if(i%2==0){
-            printf(" %d",i);
+        if( i%2 == 0 ){
+            printf(" %d ", i );
         }
     }
 }
-*******************************************************************************
+/*crie um programa que imprima os números de 10 a 1 em ordem decrescente*/
+int main(){
+
+    int i;
+    i = 10;
+
+    while( i >= 1 ){
+    printf(" %d ", i );
+    i--;
+    }
+}
+/*escreva um programa que solicite a idade do usuário e informe se ele é uma criança, adolescente, adulto ou idoso*/
+int main
 
