21d7f09dfc646ce5b06d053e723571255a2b2cb1
caiopalmardemelo <159266607+caiopalmardemelo@users.noreply.github.com>
2025-03-11 12:07:25 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,16 @@
 /*escreva um programa que imprima os números de 1 a 10 usando um loop while*/
 int main(){
-
     int i;
     i = 0;
-
     while( i <= 10 ){
-    printf("%d ", i );
-    i++;
+        printf("%d ", i );
+        i++;
     }
 }
 /*faça um programa que conte de 0 a 20, mas só imprima os números pares*/
 int main(){
-
     int i;
     i = -1;
-
     while (i < 20){
         i = i + 1;
         if( i%2 == 0 ){
@@ -24,13 +20,11 @@ int main(){
 }
 /*crie um programa que imprima os números de 10 a 1 em ordem decrescente*/
 int main(){
-
     int i;
     i = 10;
-
     while( i >= 1 ){
-    printf("%d ", i );
-    i--;
+        printf("%d ", i );
+        i--;
     }
 }
 /*escreva um programa que solicite a idade do usuário e informe se ele é uma criança, adolescente, adulto ou idoso*/
@@ -52,3 +46,17 @@ int main(){
         printf("tá com o pé na cova né véio filha da puta");
     }
 }
+/*crie um programa que solicite um número ao usuário e imprima se ele é positivo, negativo ou zero*/
+int main(){
+    int i;
+    printf("digite um número: ");
+    scanf("%d", &i );
+    if( i < 0 ){
+        printf("%d é um número negativo", i );
+    }else if( i > 0 ){
+        printf("%d é um número positivo", i );
+    }else{
+        printf("%d é ZERO!", i );
+    }
+}
+
